f745b176b4fa286c8b4e71a65c9699115523afea
Benjamín Rolando Feldman <benjafloo2007@gmail.com>
2023-11-22 08:48:16 -0300

    Major fixes 22.11

--- Models/BD.cs
+++ Models/BD.cs
@@ -2,27 +2,31 @@ using System.Data.SqlClient;
 using Dapper;
 using System.Collections.Generic;
 using System.Data;
+namespace TPFinal.Models;
 
-public class BD{
+public class BD
+{
     private static string _connectionString = @"Server=localhost;DataBase=TP09;Trusted_Connection=True;";
-    
-    public static List<Usuarios> LoginUsuario(string Nombre, string Contraseña){
-        List<Usuarios> Usuarios = null;
-        using(SqlConnection db = new SqlConnection(_connectionString)){
-            string sp = "LoginUsuario";
-            Usuarios = db.Query<Usuarios>(sp, new{Nombre = Nombre, Contraseña = Contraseña}, commandType: CommandType.StoredProcedure).ToList();
-        }
-        return Usuarios;
-    }
 
+    public static List<Usuarios> LoginUsuario(string nombreUsuario, string contraseña)
+    {
+        List<Usuarios> usuarios = null;
+
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sp = "LoginUsuario";
+            usuarios = db.Query<Usuarios>(sp, new { sp_Nombre = nombreUsuario, sp_Contraseña = contraseña }, commandType: CommandType.StoredProcedure).ToList();
+        }
+
+        return usuarios;
+    }
 
     public static void RegistrarUsuario(Usuarios usuario)
-{
-    using(SqlConnection db = new SqlConnection(_connectionString))
     {
-        string sp = "RegistrarUsuario";
-        db.Execute(sp, new { Nombre = usuario.NombreUsuario, Contraseña = usuario.Contraseña, Correo = usuario.Mail }, commandType: CommandType.StoredProcedure);
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sp = "Registrar-Usuario";
+            db.Execute(sp, new { Nombre = usuario.NombreUsuario, Contraseña = usuario.Contraseña, Correo = usuario.Mail }, commandType: CommandType.StoredProcedure);
+        }
     }
-}
-
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,37 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function() {
+    var posts = [
+        {
+            imageUrl: 'post_pic.jpg',
+            caption: 'This is a post caption',
+            time: '12:34 PM'
+        }
+    ];
 
-// Write your JavaScript code.
+    var container = document.querySelector('.container');
+
+    posts.forEach(function(post) {
+        var postDiv = document.createElement('div');
+        postDiv.className = 'post';
+
+        var img = document.createElement('img');
+        img.src = post.imageUrl;
+        img.alt = 'Post Pic';
+
+        var postInfoDiv = document.createElement('div');
+        postInfoDiv.className = 'post-info';
+
+        var caption = document.createElement('span');
+        caption.textContent = post.caption;
+
+        var time = document.createElement('span');
+        time.textContent = post.time;
+
+        postInfoDiv.appendChild(caption);
+        postInfoDiv.appendChild(time);
+
+        postDiv.appendChild(img);
+        postDiv.appendChild(postInfoDiv);
+
+        container.appendChild(postDiv);
+    });
+});
